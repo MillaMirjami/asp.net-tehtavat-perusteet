57bab8b7961343e5e6de31d205b589e03fab55cd
MillaMirjami <milla.sukki@gmail.com>
2025-01-28 13:53:41 +0200

    webapi todo kesken, luotu Controllers ja Models

--- src/todo/Program.cs
+++ src/todo/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Todo.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -35,6 +41,10 @@ app.MapGet("/weatherforecast", () =>
 })
 .WithName("GetWeatherForecast")
 .WithOpenApi();
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
 
