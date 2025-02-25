aff6330b2f5af15b1903c6b099063bb0bffca653
MillaMirjami <milla.sukki@gmail.com>
2025-02-25 14:02:18 +0200

    Program.cs:ään lisätty kommentit kaikkiin toimintoihin. Authentication lisätty kirjautumista varten. Virhesivun kokeilu

--- src/AuthorisationApi/Program.cs
+++ src/AuthorisationApi/Program.cs
@@ -1,30 +1,72 @@
 using Microsoft.EntityFrameworkCore;
 using Authorisation.Models;
-var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args); 
+// Creates webApplicationBuilder object, easy way to define and 
+// configure ASP.NET Core application.
 
-builder.Services.AddControllers();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<UserContext>(opt =>
-    opt.UseInMemoryDatabase("UserList"));
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddOpenApi();
 
-var app = builder.Build();
+// ADD SERVICES TO THE CONTAINER. //
+            //
+builder.Services.AddControllers(); // Adds (MVC = Model-View-Controllers) controllers to use
+            //
+builder.Services.AddSwaggerGen(); // Registers Swagger-services, 
+                                  // UI that displays API-documentation and 
+                                  // enables API-requests directly on browser
+            //
+builder.Services.AddDbContext<UserContext> // Adds DbContext-service to 
+                                           // dependency injection (DI) container so 
+                                           // UserContext-class can be injected to 
+                                           // other classes (ex. to controllers) 
+                                           // during program.
+                                           // UserContext represents connection to the database 
+                                           // and enables access to the db tables.
+            //
+(opt => opt.UseInMemoryDatabase("UserList")); 
+// opt is randomly chosen name for DbContextOptionsBuilder-class which helps to define db's configuration, such as type and connection settings.
+// Configures that In-Memory db is being used, name ''UserList'' is symbolic, 
+// since data is not permanently stored.
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+builder.Services.AddOpenApi(); // Registers services which creates and offers OpenAPI-doc for the app.
+                               // Enables API-doc to be in JSON- or YAML-format.
+                               // Prepares API's doc to be in the OpenAPI Spesification standard format. (OAS)
+
+var app = builder.Build(); // Build gathers all the previously defined configurations 
+                           // and creates a webApplicationBuilder object with those configs 
+                           // and is ready to accepts HTTP-requests.
+
+if (app.Environment.IsDevelopment()) // Configure the HTTP request pipeline. (Configure Swagger UI)
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwagger(); // Creates Swagger to the documentation
+    app.UseSwaggerUI(); // Creates UI for the Swagger documentation
+    app.UseDeveloperExeptionPage(); // Displays error messages & stack race info in development
 }
 
-app.UseHttpsRedirection();
+app.UseHttpsRedirection(); // Middleware, helps redirect if 
+                          // user tries http-request into https-request 
+                          // (Hypertext Transfer Protocol Secure).
 
-app.UseAuthorization();
+app.UseAuthentication(); // Middleware, checks identity
+                         // Then authorization checks which access can this identity have.
 
-app.MapControllers();
+app.UseAuthorization(); // Middleware, ensures that authorization protocols 
+                        // (roles, rigths, access) are checked before user can
+                        // access secured recourses or functions. 
 
-app.Run();
+app.MapControllers(); // Configures the routing and HTTP-requests,
+                      // Declares that HTTP-requests are routed to controllers.
+
+app.Run(); // Starts the application
+
+//  FOR MINIMAL APIS THAT DON'T USE CONTROLLERS //
+                //
+// builder.Services.AddEndpointsApiExplorer(); //
+                //
+// Searches programs API's endpoints (API-routes) 
+// and helps to create doc of it for the Swagger doc. 
+                //
+// app.MapGet("/hello", () => "Hello, World!"); // Declares API-routes
+                //
+                //
+                //
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
